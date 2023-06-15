7ad24d5196abf01b4f574c63455b6fbcfa6b58f8
amine <bouramtaneamine1@gmail.com>
2023-06-15 23:18:14 +0100

    add controller,model,migration

---
+++ database/migrations/2023_06_15_220828_create_annonces_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('annonces', function (Blueprint $table) {
+            $table->id();
+            $table->string("titre",200);
+            $table->text("description");
+            $table->enum("type",["Terain"," Appartemant","Maison","Appartement"]);
+            $table->string("ville",200);
+            $table->integer("superficie");
+            $table->boolean("neuf");
+            $table->decimal("prix");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('annonces');
+    }
+};
